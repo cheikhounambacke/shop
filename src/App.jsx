d2b2763601069d152a4ccdc3c6e0907c955256aa
“Your <cheikhounambacke646@gmail.com>
2024-10-09 23:20:35 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { products } from "./assets/assets";
+const App = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default App;
